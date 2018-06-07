95078db345a1601496943bca63ed4c8bed9b52dc
Matthew McNeil <mtthwmcneil@gmail.com>
2018-06-07 20:20:01 +0000

    optbuild.py edited online with Bitbucket

--- lib/optbuild.py
+++ lib/optbuild.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-from __future__ import division
-
-from __future__ import absolute_import
-from __future__ import print_function
+from __future__ import division, absolute_import, print_function
 import six
 __version__ = "$Revision: 1.30 $"
 
